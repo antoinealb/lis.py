ec6df2072558c9bde7e06b22df1fa8f9d5bbff2e
Antoine Albertelli <antoinea101@gmail.com>
2014-08-18 01:46:58 +0200

    Separate interpreter from LISP library

--- lispy.py
+++ lispy.py
@@ -188,27 +188,3 @@ def input_expression():
 
     return ' '.join(result)
 
-def main():
-    import sys
-    env = create_base_env()
-
-    if len(sys.argv) > 1:
-        with open(sys.argv[1]) as f:
-            prog = create_prog(f.read())
-
-        eval_prog(prog, env)
-
-    else:
-        while True:
-            prog = input_expression()
-            prog = create_prog(prog)
-            result = eval_prog(prog, env)
-
-            if result is not None:
-                print("=> {}".format(result))
-
-
-if __name__ == "__main__":
-    main()
-
-
