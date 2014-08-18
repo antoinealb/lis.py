5aa54122d08bf19a7a36a78d56aeb29db8623d3a
Antoine Albertelli <antoinea101@gmail.com>
2014-08-18 03:14:11 +0200

    Add a regression test script

---
+++ regression_testing.lisp
@@ -0,0 +1,13 @@
+#!./interpreter.py
+(require "unittest.lisp")
+
+; Some simple sanity tests
+(shouldEqual 4 (+ 1 3) "Adding two numbers")
+(shouldEqual 4 (car (list 4 3 2)) "Taking the head of a list")
+(shouldEqual (list 3 2) (cdr (list 4 3 2)) "Taking the tail of a list")
+
+; Some lambda test
+(set! increment (lambda (x) (+ x 1)))
+(shouldEqual (increment 3) 4 "Simple incrementer lambda")
+
+(testEnd)
